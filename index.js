4ba89307b3f7d366f3bf4c1d7a627a58cd933603
Massimo M <54358568+MaxPHPdeveloper@users.noreply.github.com>
2020-11-25 14:43:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ client.once("ready", () => {
 client.on("message", async message => {
     if (message.content.startsWith("!play")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`)
-        if (!message.guild.voiceConnection) message.member.voiceChannel.join();
+        const connection = message.member.voice.channel.join();
         if (!message.content.includes("https://www.youtube.com/" || "www.youtube.com")) {
             const arg = message.content.replace("!play ", "");
             const r = await yts(arg);
             const videos = r.videos.slice(0, 1);
             const url = videos.url;
+            connection.play(ytdl(url, { filter: "audioonly" }));
+        }
+        else {
+            const url = message.content.replace("!play ", "");
             connection.play(ytdl(url, { filter: "audioonly" }));
         }
     }
@@ -38,7 +42,7 @@ client.on("message", async message => {
         //da finire, manca la queue da stoppare
     }
     else if (message.content.startsWith("!help")) {
-        return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t| fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t| per fermare la coda\n\nASSGHARAAA`);
+        return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t\t| fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t\t\t\t| per fermare la coda\n\nASSGHARAAA`);
     }
     else if (!message.author.bot) return message.channel.send(`Coooos? Usa !help per scoprire tutti i comandi disponibili.`);
 });
