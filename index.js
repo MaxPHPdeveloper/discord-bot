8d6d7a1b1f3cefc40276f0d7f4908a0ac09ba552
Massimo M <54358568+MaxPHPdeveloper@users.noreply.github.com>
2020-11-25 22:09:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,25 +11,57 @@ client.once("ready", () => {
     console.log("Ready!");
 });
 
-if (queue.length > 0) {
-    play(queue[d]);
+function play(queue, d) {
+    message.member.voice.channel.join().then(connection => {
+        connection.play(ytdl(queue[d], { filter: "audioonly" }).on("end", function () {
+            if (queue[d + 1] != undefined) {
+                play(queue, d + 1);
+            }
+            else {
+                connection.disconnect;
+                clearQueue();
+            }
+        }));
+    });
 }
+
+function clearQueue() {
+    for (var p = 0; p < queue.length; p++) {
+        queue[p] = undefined;
+    }
+    d = 0;
+    return;
+}
+
+if (queue[0] != undefined) play(queue, 0);
 
 client.on("message", async message => {
     if (message.content.startsWith("!play")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`);
+
+        /*
+        //se non è un link
         if (!message.content.includes("https://www.youtube.com/" || "www.youtube.com")) {
             var arg = message.content.replace("!play ", "");
             const r = await yts(arg);
             const videos = r.videos.slice(0, 1);
             var url = videos.url;
-            addQueue(url);
+            message.member.voice.channel.join().then(connection => {
+                connection.play(ytdl(url, { filter: "audioonly" }));
+            });
         }
+        */
+
+        //se è un link
         else {
             var url = message.content.replace("!play ", "");
-            addQueue(url);
+            queue[d] = url;
+            d++;
+            return;
         }
     }
+
+
     else if (message.content.startsWith("!youtube")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`);
         var arg = message.content.replace("!youtube ", "");
@@ -48,61 +80,48 @@ client.on("message", async message => {
         let collected = await message.channel.awaitMessages(filter, { max: 1, time: 60000, });
         switch (collected.first().content) {
             case "1":
-                addQueue(links[1]);
-                message.channel.send(`Added to queue.`);
+                queue[d] = links[1];
+                d++;
                 break;
             case "2":
-                addQueue(links[2]);
-                message.channel.send(`Added to queue.`);
+                queue[d] = links[2];
+                d++;
                 break;
             case "3":
-                addQueue(links[3]);
-                message.channel.send(`Added to queue.`);
+                queue[d] = links[3];
+                d++;
                 break;
             case "4":
-                addQueue(links[4]);
-                message.channel.send(`Added to queue.`);
+                queue[d] = links[4];
+                d++;
                 break;
             case "5":
-                addQueue(links[5]);
-                message.channel.send(`Added to queue.`);
+                queue[d] = links[5];
+                d++;
                 break;
 
             default:
                 return message.channel.send(`Selezione non valida.`);
                 break;
         }
+        return;
     }
+
+
+
     else if (message.content.startsWith("!stop")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`);
-        if (queue.length > 0) stopQueue();
-        else return message.channel.send(`La coda è vuota.`);
+        if (message.guild.connection) message.guild.voiceConnection.disconnect();
     }
+
+
     else if (message.content.startsWith("!help")) {
         return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t\t | fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t\t\t              | per fermare la coda\n\nASSGHARAAA`);
     }
 });
-function play(url) {
-    message.member.voice.channel.join().then(connection => {
-        connection.play(ytdl(url, { filter: "audioonly" }));
-    });
-}
-function addQueue(link) {
-    queue[d] = link;
-    d++;
-}
 
-function removeQueue() {
-    if (queue[0] != undefined) {
-        queue[d] = undefined;
-        d--;
-    }
-}
 
-function stopQueue() {
-    for (var p = 0; p < queue.length; p++) {
-        queue[p] = undefined;
-    }
-}
+
+
 
 client.login(process.env.TOKEN);
