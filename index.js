b7ed794c21c43dde0449a6043d4cc863f5bb0cf0
Massimo M <54358568+MaxPHPdeveloper@users.noreply.github.com>
2020-11-25 22:24:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,30 +11,6 @@ client.once("ready", () => {
     console.log("Ready!");
 });
 
-function play(queue, d) {
-    message.member.voice.channel.join().then(connection => {
-        connection.play(ytdl(queue[d], { filter: "audioonly" }).on("end", function () {
-            if (queue[d + 1] != undefined) {
-                play(queue, d + 1);
-            }
-            else {
-                connection.disconnect;
-                clearQueue();
-            }
-        }));
-    });
-}
-
-function clearQueue() {
-    for (var p = 0; p < queue.length; p++) {
-        queue[p] = undefined;
-    }
-    d = 0;
-    return;
-}
-
-if (queue[0] != undefined) play(queue, 0);
-
 client.on("message", async message => {
     if (message.content.startsWith("!play")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`);
@@ -56,8 +32,8 @@ client.on("message", async message => {
         else {
             var url = message.content.replace("!play ", "");
             queue[d] = url;
+            play(queue, d);
             d++;
-            return;
         }
     }
 
@@ -81,42 +57,80 @@ client.on("message", async message => {
         switch (collected.first().content) {
             case "1":
                 queue[d] = links[1];
+                play(queue, d);
                 d++;
+                message.channel.send(`Added to queue.`);
                 break;
             case "2":
                 queue[d] = links[2];
+                play(queue, d);
                 d++;
+                message.channel.send(`Added to queue.`);
                 break;
             case "3":
                 queue[d] = links[3];
+                play(queue, d);
                 d++;
+                message.channel.send(`Added to queue.`);
                 break;
             case "4":
                 queue[d] = links[4];
+                play(queue, d);
                 d++;
+                message.channel.send(`Added to queue.`);
                 break;
             case "5":
                 queue[d] = links[5];
+                play(queue, d);
                 d++;
+                message.channel.send(`Added to queue.`);
                 break;
 
             default:
                 return message.channel.send(`Selezione non valida.`);
                 break;
         }
-        return;
     }
 
 
 
     else if (message.content.startsWith("!stop")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`);
-        if (message.guild.connection) message.guild.voiceConnection.disconnect();
+        if (message.guild.connection) {
+            message.channel.send(`Lascio a voi le mie fike bianke, prendetevene cura. Bella a tutti.`);
+            message.guild.voiceConnection.disconnect();
+            clearQueue();
+        }
+
     }
 
 
     else if (message.content.startsWith("!help")) {
         return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t\t | fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t\t\t              | per fermare la coda\n\nASSGHARAAA`);
+    }
+
+
+    function clearQueue() {
+        for (var p = 0; p < queue.length; p++) {
+            queue[p] = undefined;
+        }
+        d = 0;
+        return;
+    }
+
+    function play(queue, d) {
+        console.log(queue);
+        message.member.voice.channel.join().then(connection => {
+            connection.play(ytdl(queue[d], { filter: "audioonly" }).on("end", function () {
+                if (queue[d + 1] != undefined) {
+                    play(queue, d + 1);
+                }
+                else {
+                    connection.disconnect;
+                    clearQueue();
+                }
+            }));
+        });
     }
 });
 
