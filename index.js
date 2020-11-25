a21d63e35feb24a439415ca75ad99555c353a1c4
Massimo M <54358568+MaxPHPdeveloper@users.noreply.github.com>
2020-11-25 18:28:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ client.once("ready", () => {
 client.on("message", async message => {
     if (message.content.startsWith("!play")) {
         if (!message.member.voice.channel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`)
-        const connection = await message.member.voice.channel.join();
         if (!message.content.includes("https://www.youtube.com/" || "www.youtube.com")) {
             var arg = message.content.replace("!play ", ""), r = await yts(arg), videos = r.videos.slice(0, 1), url = String(videos.url);
-            connection.play(ytdl(url, { filter: "audioonly" }));
+            message.member.voice.channel.join().then(connection => {
+                connection.play(ytdl(url, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+            });
         }
         else {
             var url = message.content.replace("!play ", "");
-            connection.play(ytdl(url, { filter: "audioonly" }));
+            message.member.voice.channel.join().then(connection => {
+                connection.play(ytdl(url, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+            });
         }
     }
     else if (message.content.startsWith("!youtube")) {
@@ -29,16 +32,64 @@ client.on("message", async message => {
             message.channel.send(`${i}° | ${v.title} (${v.timestamp}) | ${v.author.name}`);
             i++;
         })
-        //da finire, manca il player
+        message.channel.send(`Scegli un numero da 1 a 5.`);
+        const filter = m => m.author.id === message.author.id;
+        message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 10000,
+        }).then(async (collected) => {
+            switch (collected.first().content) {
+                case "1":
+                    var video1 = r.videos.slice(0, 1);
+                    var url1 = String(video1.url);
+                    message.member.voice.channel.join().then(connection => {
+                        connection.play(ytdl(url1, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+                    });
+                    break;
+                case "2":
+                    var video2 = r.videos.slice(1, 2);
+                    var url2 = String(video2.url);
+                    message.member.voice.channel.join().then(connection => {
+                        connection.play(ytdl(url2, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+                    });
+                    break;
+                case "3":
+                    var video3 = r.videos.slice(2, 3);
+                    var url3 = String(video3.url);
+                    message.member.voice.channel.join().then(connection => {
+                        connection.play(ytdl(url3, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+                    });
+                    break;
+                case "4":
+                    var video4 = r.videos.slice(3, 4);
+                    var url4 = String(video4.url);
+                    message.member.voice.channel.join().then(connection => {
+                        connection.play(ytdl(url4, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+                    });
+                    break;
+                case "5":
+                    var video5 = r.videos.slice(4, 5);
+                    var url5 = String(video5.url);
+                    message.member.voice.channel.join().then(connection => {
+                        connection.play(ytdl(url5, { filter: "audioonly" }).on("finish", () => connection.disconnect()));
+                    });
+                    break;
+
+                default:
+                    return message.channel.send(`Selezione non valida.`);
+                    break;
+            }
+        }).catch(() => {
+            return message.channel.send('Hai esitato troppo fratello, la fika bianka non aspettate te.');
+        });
     }
     else if (message.content.startsWith("!stop")) {
         if (!message.member.voiceChannel) return message.channel.send(`Devi essere in un canale vocale per usare questo comando.`)
         //da finire, manca la queue da stoppare
     }
     else if (message.content.startsWith("!help")) {
-        return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t\t | fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t\t\t         | per fermare la coda\n\nASSGHARAAA`);
+        return message.channel.send(`No Billie ascolta...con kuesto locdaun si tromba a fatica ehhh. Comandi disponibili:\n\n\n1) !play [link] oppure [nome video]\t\t\t| per ascoltare musica\n2) !youtube [nome video]\t\t\t\t\t\t\t | fornisce i primi 5 risultati di youtube\n3) !stop\t\t\t\t\t\t\t\t\t\t\t\t             | per fermare la coda\n\nASSGHARAAA`);
     }
-    else if (!message.author.bot) return message.channel.send(`Coooos? Usa !help per scoprire tutti i comandi disponibili.`);
 });
 
 /*
