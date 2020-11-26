4d390b32f19d907064c72f4983171c6457fc2d84
Massimo M <54358568+MaxPHPdeveloper@users.noreply.github.com>
2020-11-26 12:45:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on("ready", () => {
     //It will count all voice channels in which bot is connected, if none it will return 0
     let playing = client.voice.connections.size; 
     //It will set the bot status to streaming
-    client.user.setPresence({ activity: { name: `music on ${playing}`, type: "STREAMING", url: "https://twitch.tv/hydroxin" } })
+    client.user.setPresence({status: "dnd", activity: { name: "Alla cavallina con le fike bianke", type: "PLAYING"}});
 
 });
 
